cd30af902fdbd33b98be59812878eb52d8c10283
Andrew Souza <andrewesouza@gmail.com>
2021-06-09 22:30:22 -0700

    still trying to understand issue with adding event to squares

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const rainbowBtn = document.createElement('button')
 const eraseBtn = document.createElement('button');
 const resetBtn = document.createElement('button');
 
-
-
 buttons.appendChild(grayBtn);
 grayBtn.textContent = 'Gray';
 buttons.appendChild(rainbowBtn);
@@ -19,18 +17,20 @@ eraseBtn.textContent = 'Erase';
 buttons.appendChild(resetBtn);
 resetBtn.textContent = 'Reset';
 
-
+window.addEventListener('load', populateSquares(16));
+window.addEventListener('load', gray);
 
 function populateSquares(num) {
     for (let i = 0; i < (num * num); i++) {
         const square = document.createElement('div');
-        square.style.border = '1px none black';
         square.style.backgroundColor = 'white';
         container.appendChild(square).classList.add('square');
         container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
     }
 }
+
+// let squares = document.querySelectorAll('.square');
 
 function gray() {
     for (let i = 0; i < squares.length; i++) {
@@ -68,10 +68,6 @@ function erase() {
     }
 }
 
-
-
-
-
 grayBtn.addEventListener('click', gray);
 rainbowBtn.addEventListener('click', rainbow);
 blackBtn.addEventListener('click', black)
@@ -80,13 +76,10 @@ resetBtn.addEventListener('click', () => {
     for (let i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = `white`;
     }
-    let gridSize = prompt('Enter a number')
-    if (gridSize < 1 || gridSize > 100 || Number.isNaN(gridSize)) {
-        gridSize = prompt('Enter a NUMBER between 1 and 100');
+    let gridSize = prompt('Enter a number between 4 and 64')
+    if (gridSize < 4 || gridSize > 64 || Number.isNaN(gridSize)) {
+        gridSize = prompt('Enter a NUMBER between 4 and 64');
     } else populateSquares(gridSize);
 })
 
-populateSquares(16);
-window.addEventListener('load', gray);
-
-const squares = document.querySelectorAll('.square');
+const squares = document.querySelectorAll('.square')
